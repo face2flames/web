d6ff0dd3a9e6b7781fec663d0f4af376f3341e4b
Face The-Flames <facetheflames@yandex.ru>
2016-02-29 10:58:53 +0600

    экспериментируем с gunicorn

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+gunicorn -D -b 0.0.0.0:8080 hello:app
