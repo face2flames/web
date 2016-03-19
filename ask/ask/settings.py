0fd3ce12e39cce33138b68e20ec34adc93368c89
Face The-Flames <facetheflames@yandex.ru>
2016-03-19 17:56:18 +0600

    Пытаюсь в Django Models

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
+    'ask',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,10 +59,14 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_kabak',
+        'USER': 'kabak',
+        'PASSWORD': 'moonpie',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
-}
+} 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
